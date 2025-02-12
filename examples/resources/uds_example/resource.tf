f04209ee10dc775e582fec3bce2209872dae7ea1
catsby <clint@defenseunicorns.com>
2025-02-12 10:16:30 -0600

    update example resource
    Signed-off-by: catsby <clint@defenseunicorns.com>

--- examples/resources/uds_example/resource.tf
+++ examples/resources/uds_example/resource.tf
@@ -1,2 +1,23 @@
-resource "uds_example" "package-name" {
+terraform {
+  required_providers {
+    uds = {
+      source  = "defenseunicorns/uds"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "uds" {}
+
+resource "uds_bundle_metadata" "example_bundle" {
+  version      = "0.0.1"
+  kind         = "UDSBundle"
+  description  = "A demo bundle for the podinfo and nginx packages"
+  architecture = "arm64"
+}
+
+resource "uds_package" "podinfo" {
+  repository   = "ghcr.io/defenseunicorns/uds-cli/podinfo"
+  ref          = "0.0.1"
+  architecture = uds_bundle_metadata.example_bundle.architecture
 }
