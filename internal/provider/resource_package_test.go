561edbdda2e9bbae1258cf8614664974a9dcd906
catsby <clint@defenseunicorns.com>
2025-02-14 16:59:49 -0600

    add basic acceptance tests
    Signed-off-by: catsby <clint@defenseunicorns.com>

--- internal/provider/resource_package_test.go
+++ internal/provider/resource_package_test.go
@@ -8,7 +8,48 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
+
+func TestAccPackageResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccPackageResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uds_bundle_metadata.example_bundle", "version", "0.0.1"),
+					resource.TestCheckResourceAttr("uds_package.init", "metadata.version", "v0.48.0"),
+					// resource.TestCheckResourceAttr("uds_package.podinfo", "version", "0.0.1"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+var testAccPackageResourceConfig = `
+resource "uds_bundle_metadata" "example_bundle" {
+  version      = "0.0.1"
+  kind         = "UDSBundle"
+  description  = "A demo bundle for the podinfo and nginx packages"
+  architecture = "arm64"
+}
+
+resource "uds_package" "init" {
+  repository   = "oci://ghcr.io/zarf-dev/packages/init:v0.48.0"
+  ref          = "v0.48.0"
+  architecture = uds_bundle_metadata.example_bundle.architecture
+}
+//
+// resource "uds_package" "podinfo" {
+//   repository   = "oci://ghcr.io/defenseunicorns/uds-cli/podinfo:0.0.2"
+//   ref          = "0.0.2"
+//   architecture = uds_package.init.architecture
+// }
+`
 
 // Unit test for flattenOverrides function
 func TestFlattenOverrides(t *testing.T) {
