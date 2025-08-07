52c4ae598e4dc33580a575fbab132a243a8fe05c
Erickson Moskito <erickson@defenseunicorns.com>
2025-08-07 03:29:20 -0700

    chore: moved acceptance tests to test/acc to make room for provider resource unit tests

--- internal/provider/resource_package_test.go
+++ internal/provider/resource_package_test.go
@@ -4,48 +4,11 @@
 package provider
 
 import (
-	"fmt"
 	"reflect"
-	"runtime"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
-
-func TestAccPackageResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: testAccPackageResourceConfig,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("uds_bundle_metadata.example_bundle", "version", "0.0.1"),
-					resource.TestCheckResourceAttr("uds_package.init", "metadata.version", "v0.48.0"),
-				),
-			},
-			// Delete testing automatically occurs in TestCase
-		},
-	})
-}
-
-var testAccPackageResourceConfig = fmt.Sprintf(`
-resource "uds_bundle_metadata" "example_bundle" {
-  version      = "0.0.1"
-  kind         = "UDSBundle"
-  description  = "A demo bundle for the podinfo and nginx packages"
-  architecture = "%s"
-}
-
-resource "uds_package" "init" {
-  name         = "init"
-  repository   = "ghcr.io/zarf-dev/packages/init"
-  ref          = "v0.48.0"
-  architecture = uds_bundle_metadata.example_bundle.architecture
-}
-`, runtime.GOARCH)
 
 // Unit test for flattenOverrides function
 func TestFlattenOverrides(t *testing.T) {
---
+++ test/acc/bundle_metadata_test.go
@@ -0,0 +1,37 @@
+// Copyright 2024 Defense Unicorns
+// SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
+package acc
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+// this test assumes a k3d cluster running.
+func TestAccMetadataResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccMetadataResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uds_bundle_metadata.example_bundle", "version", "0.0.1"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+var testAccMetadataResourceConfig = `
+resource "uds_bundle_metadata" "example_bundle" {
+  version      = "0.0.1"
+  kind         = "UDSBundle"
+  description  = "A demo bundle for the podinfo and nginx packages"
+  architecture = "arm64"
+}
+`
