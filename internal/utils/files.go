ff1249f6f60231ed749351d8a5fbcdc945fee8e3
Erickson Moskito <erickson@defenseunicorns.com>
2025-09-16 22:45:02 -0700

    chore: add utility function to create temporary public key file

---
+++ internal/utils/files.go
@@ -0,0 +1,25 @@
+// Copyright 2024 Defense Unicorns
+// SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+// CreateTempPublicKeyFile creates temporary file containing the public key.
+func CreateTempPublicKeyFile(publicKey string) (string, error) {
+	tmpFile, err := os.CreateTemp("", "*.pub")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temporary public key file: %w", err)
+	}
+	defer tmpFile.Close()
+
+	if _, err := tmpFile.WriteString(publicKey); err != nil {
+		os.Remove(tmpFile.Name())
+		return "", fmt.Errorf("failed to write public key to temporary file: %w", err)
+	}
+
+	return tmpFile.Name(), nil
+}
