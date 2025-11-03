208a48795a9a1feb52c5d444741727e5eb200a58
Erickson Moskito <erickson@defenseunicorns.com>
2025-11-03 09:57:31 -0800

    chore: added comment with yamlencode usage example

--- examples/resources/uds_example/resource.tf
+++ examples/resources/uds_example/resource.tf
@@ -60,7 +60,7 @@ resource "uds_package" "podinfo" {
       values = [
         { path = "replicaCount", value = "3" },
         # Use single quotes or yamlencode function in single-line strings to escape special HCL characters ("#").
-        { path = "ui.color", value = "'#663399'" }
+        { path = "ui.color", value = "'#663399'" } # Could also use: value = yamlencode("#663399")
       ]
     }
   }
