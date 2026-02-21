92ffa882b236eee862110be3605eb88cb658b93b
Erickson Moskito <erickson@defenseunicorns.com>
2026-02-20 18:31:01 -0800

    chore: updated uds_package examples with import

---
+++ examples/resources/uds_package/import.sh
@@ -0,0 +1,5 @@
+# Import a package deployed without a namespace override
+tofu import uds_package.init init
+
+# Import a package deployed with a namespace override
+tofu import uds_package.demo_dos_games demo:dos-games
