11d52fd73c34af49eb9006f4a5715afc0d97283a
Erickson Moskito <erickson@defenseunicorns.com>
2026-06-04 00:35:19 -0700

    chore: fix acceptance test

--- test/acc/package_test.go
+++ test/acc/package_test.go
@@ -20,10 +20,10 @@ resource "uds_package" "init" {
   source       = "oci://ghcr.io/zarf-dev/packages/init:%s"
   architecture = "%s"
   signature_verification = {
-	keyless = {
-	  certificate_identity_regexp = "https://github\\.com/zarf-dev/zarf/\\.github/workflows/release\\.yml@refs/tags/v\\d+\\.\\d+\\.\\d+"
+    keyless = {
+      certificate_identity_regexp = "https://github\\.com/zarf-dev/zarf/\\.github/workflows/release\\.yml@refs/tags/v\\d+\\.\\d+\\.\\d+"
       certificate_oidc_issuer     = "https://token.actions.githubusercontent.com"
-	}
+    }
   }
 }
 `, initPackageVersion, runtime.GOARCH)
@@ -59,27 +59,24 @@ resource "uds_package" "init" {
   source       = "oci://ghcr.io/zarf-dev/packages/init:%s"
   architecture = "%s"
   signature_verification = {
-	keyless = {
-	  certificate_identity_regexp = "https://github\\.com/zarf-dev/zarf/\\.github/workflows/release\\.yml@refs/tags/v\\d+\\.\\d+\\.\\d+"
+    keyless = {
+      certificate_identity_regexp = "https://github\\.com/zarf-dev/zarf/\\.github/workflows/release\\.yml@refs/tags/v\\d+\\.\\d+\\.\\d+"
       certificate_oidc_issuer     = "https://token.actions.githubusercontent.com"
-	}
-  }
-  component {
-    name = "git-server"
+    }
   }
 }
 
 resource "uds_package" "dos_games" {
+  depends_on   = [uds_package.init]
   source       = "oci://ghcr.io/zarf-dev/packages/dos-games:1.2.0"
   architecture = uds_package.init.architecture
   namespace    = "demo"
-  depends_on   = [uds_package.init]
   signature_verification = {
-  	public_key = file("%s")
+    public_key = file("%s")
   }
 }
 
-# TODO: Add resource with local package reference
+# TODO: Add uds_package resource with local package reference
 `, initPackageVersion, runtime.GOARCH, pubKeyPath)
 }
 
@@ -92,10 +89,10 @@ func TestAccMultiplePackageResources(t *testing.T) {
 			{
 				Config: testAccMultiplePackageResourcesConfig(t),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("uds_package.init", "metadata.version", initPackageVersion),
 					resource.TestCheckResourceAttr("uds_package.init", "id", "init"),
-					resource.TestCheckResourceAttr("uds_package.dos_games", "metadata.version", "1.2.0"),
+					resource.TestCheckResourceAttr("uds_package.init", "metadata.version", initPackageVersion),
 					resource.TestCheckResourceAttr("uds_package.dos_games", "id", "demo:dos-games"),
+					resource.TestCheckResourceAttr("uds_package.dos_games", "metadata.version", "1.2.0"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
