35c09111329b86b922a5218fdc64bbd0a26b9200
Joel McCoy <joel@defenseunicorns.com>
2026-07-24 15:04:49 -0500

    chore: more renovate/release please cleanup/fixes

--- examples/zarf_values/main.tf
+++ examples/zarf_values/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     uds = {
       source  = "defenseunicorns/uds"
-      version = "~> 0.4.0"
+      version = "~> 0.4.0" # x-release-please-version
     }
   }
 }
