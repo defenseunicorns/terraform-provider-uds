10bacc2bcf4af1ff1f567fdb1bcfa257c39a7aa0
Joel McCoy <joel@defenseunicorns.com>
2026-07-31 13:34:18 -0500

    chore: add back in some provider examples

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -6,4 +6,13 @@ terraform {
   }
 }
 
-provider "uds" {}
+provider "uds" {
+  # Override the architecture detected from the local machine.
+  # default_architecture = "amd64"
+
+  # Use a custom directory for downloaded packages.
+  # zarf_cache_path = "~/.zarf-cache"
+
+  # Disable package loading and validation during planning.
+  # validate_packages_on_plan = false
+}
